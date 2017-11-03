a2a00f57eefba2bf07a752cc7fde4532a916fb3f
jannesbrunner <jannes.brunner@student.htw-berlin.de>
2017-11-03 17:19:48 +0100

    Set up blank screens and entry point

---
+++ app/screens/NewContact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class NewContact extends Component {
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>New Contact Screen</Text>
+            </View>
+        );
+    }
+}
+export default NewContact;
